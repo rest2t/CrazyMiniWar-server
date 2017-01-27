15faf90ab71d35b25db4e18cc25de97e6e9daa63
rest2t <rest2t@DESKTOP-6TBS1CP>
2017-01-27 22:43:12 +0100

    Initial D tududuudud

--- CrazyMiniWar-server/Program.cs
+++ CrazyMiniWar-server/Program.cs
@@ -1,15 +1,51 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
 
-namespace CrazyMiniWar_server
+public class serv
 {
-    class Program
+    public static void Main()
     {
-        static void Main(string[] args)
+        try
         {
+            IPAddress ipAd = IPAddress.Parse("127.0.0.1");
+            // use local m/c IP address, and 
+            // use the same in the client
+
+            /* Initializes the Listener */
+            TcpListener myList = new TcpListener(ipAd, 8001);
+
+            /* Start Listeneting at the specified port */
+            myList.Start();
+
+            Console.WriteLine("The server is running at port 8001...");
+            Console.WriteLine("The local End point is  :" +
+                              myList.LocalEndpoint);
+            Console.WriteLine("Waiting for a connection.....");
+
+            Socket s = myList.AcceptSocket();
+            Console.WriteLine("Connection accepted from " + s.RemoteEndPoint);
+
+            byte[] b = new byte[100];
+            int k = s.Receive(b);
+            Console.WriteLine("Recieved...");
+            for (int i = 0; i < k; i++)
+                Console.Write(Convert.ToChar(b[i]));
+
+            ASCIIEncoding asen = new ASCIIEncoding();
+            s.Send(asen.GetBytes("The string was recieved by the server."));
+            Console.WriteLine("\nSent Acknowledgement");
+            /* clean up */
+            s.Close();
+            myList.Stop();
+            Console.Read();
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Error..... " + e.StackTrace);
         }
     }
+
 }

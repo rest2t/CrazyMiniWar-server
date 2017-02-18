d6ef1097da32c90671561d30b54a59fb1bd76cc2
ja <groxor123@gmail.com>
2017-02-18 19:03:05 +0100

    player synchronization

---
+++ CrazyMiniWar-server/Client.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrazyMiniWar_server
+{
+    class Client
+    {
+        public TcpClient Tcp;
+        public StreamWriter Writer;
+        public StreamReader Reader;
+        public String Name;
+    }
+}
--- CrazyMiniWar-server/Program.cs
+++ CrazyMiniWar-server/Program.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Text;
 using System.Net;
 using System.Net.Sockets;
 using System.Threading;
 using System.IO;
+using CrazyMiniWar_server;
 
 public class serv
 {
@@ -18,9 +20,11 @@ class TcpServer
 {
     private TcpListener _server;
     private Boolean _isRunning;
+    private List<Client> _clients;
 
     public TcpServer(int port)
     {
+        _clients = new List<Client>();
         _server = new TcpListener(IPAddress.Any, port);
         _server.Start();
 
@@ -35,35 +39,88 @@ class TcpServer
         {
             TcpClient newClient = _server.AcceptTcpClient();
             Thread t = new Thread(new ParameterizedThreadStart(HandleClient));
-            t.Start(newClient);
+            Client client = new Client();
+            client.Tcp = newClient;
+
+            t.Start(client);
         }
     }
 
     public void HandleClient(object obj)
     {
         // retrieve client from parameter passed to thread
-        TcpClient client = (TcpClient)obj;
-        Console.WriteLine("New client: " + ((IPEndPoint)client.Client.RemoteEndPoint).Address.ToString());
+        Client client = (Client)obj;
+        //Console.WriteLine("New client: " + ((IPEndPoint)client.Tcp.Client.RemoteEndPoint).Address.ToString());
+
+
         // sets two streams
-        StreamWriter sWriter = new StreamWriter(client.GetStream(), Encoding.ASCII);
-        StreamReader sReader = new StreamReader(client.GetStream(), Encoding.ASCII);
+        client.Writer = new StreamWriter(client.Tcp.GetStream(), Encoding.ASCII);
+        client.Reader = new StreamReader(client.Tcp.GetStream(), Encoding.ASCII);
         // you could use the NetworkStream to read and write, 
         // but there is no forcing flush, even when requested
-
+            _clients.Add(client);
         Boolean bClientConnected = true;
         String sData = null;
 
         while (bClientConnected)
         {
             // reads from stream
-            sData = sReader.ReadLine();
+            try
+            {
+                sData = client.Reader.ReadLine();
+                if (sData != null)
+                {
+                    String[] args = sData.Split('|');
+                    if (sData.IndexOf("setName") == 0)
+                    {
+                        client.Name = args[1];
+                        Console.WriteLine("new name: " + client.Name);
+                        BroadCast(client, "newClient|" + client.Name);
+                        foreach (var client1 in _clients)
+                        {
+                            if (client1 != client)
+                            {
+                                client.Writer.WriteLine("newClient|" + client1.Name);
+
+                            }                        }
+                    }
+                    if (sData.IndexOf("pos") == 0)
+                    {
+                       BroadCast(client, "pos|" + args[1] + "|" + args[2] + "|" + client.Name);
+                    }
+                }
+            }
+            catch(IOException exception)
+            {
+                bClientConnected = false;
+            }
+            
 
             // shows content on the console.
-            Console.WriteLine("Client:  " + ((IPEndPoint)client.Client.RemoteEndPoint).Address.ToString() + " " + sData);
+            //Console.WriteLine("Client:  " + ((IPEndPoint)client.Tcp.Client.RemoteEndPoint).Address.ToString() + " " + sData);
 
             // to write something back.
             // sWriter.WriteLine("Meaningfull things here");
             // sWriter.Flush();
         }
+        Console.WriteLine("client disconnected: " + client.Name);
+        _clients.Remove(client);
+        foreach (var client1 in _clients)
+        {
+            client1.Writer.WriteLine("disconnect|" + client.Name);
+            client1.Writer.Flush();
+        }
+    }
+
+    private void BroadCast(Client curr, String msg)
+    {
+         foreach (var client1 in _clients)
+         {
+            if (client1 != curr)
+            {
+                client1.Writer.WriteLine(msg);
+                client1.Writer.Flush();
+            }
+         }
     }
 }
